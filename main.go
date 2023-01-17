434fa5c539404e6fc29c69afd10ebd051b613490
Oluwafunmilayo  Olayiwola <funmilayoolayiwola03@gmail.com>
2023-01-17 19:50:41 +0100

    modified register-api

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
+
+	//"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -16,28 +17,32 @@ import (
 //User structs
 
 type User struct {
-	Name       string `json:"Fullname"`
+	Name       string `json:"name"`
 	Profession string `json:"profession"`
 	Location   string `json:"location"`
 }
 
 var register []User
 
-func getDetails(w http.ResponseWriter, r *http.Request) {
+func getDetail(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	p := mux.Vars(r) //get paramas
 
 	for _, detail := range register {
 		if detail.Name == p["name"] {
-			detail.Name = strings.ToLower(detail.Name)
+			//detail.Name = strings.ToLower(detail.Name)
 			json.NewEncoder(w).Encode(detail)
 			return
 		}
 	}
-
 	json.NewEncoder(w).Encode(&User{})
-
 }
+
+func getAllDetails(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "applicatiom/json")
+	json.NewEncoder(w).Encode((register))
+}
+
 func createAccount(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var user User
@@ -45,17 +50,21 @@ func createAccount(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	register = append(register, user)
+	json.NewEncoder(w).Encode(user)
 }
 
 func main() {
 	//initializing Router
 	h := mux.NewRouter()
+
+	//mock data to test database
 	register = append(register, User{Name: "Funmi", Profession: "trader", Location: "Abuja"})
-	register = append(register, User{Name: "wunmi", Profession: "doctor", Location: "lagos"})
+	register = append(register, User{Name: "Wunmi", Profession: "doctor", Location: "lagos"})
 	//creating a server with two endpoints and route Hnadlers
 	h.HandleFunc("/api/register/", createAccount).Methods("POST")
-	h.HandleFunc("/api/user/{name}", getDetails).Methods("GET")
+	h.HandleFunc("/api/user/{name}", getDetail).Methods("GET")
+	h.HandleFunc("/api/registered", getAllDetails).Methods("GET")
 
 	s := &http.Server{
 		Addr:           ":8080",
